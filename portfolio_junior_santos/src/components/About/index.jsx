4fcdd306699f9bac4e7ff7a3586c2ff26bb59bd4
Junior Santos <nuno.ribeiro05@hotmail.com>
2024-01-16 10:14:31 -0300

    Subindo algumas atualizações do curriculo

--- portfolio_junior_santos/src/components/About/index.jsx
+++ portfolio_junior_santos/src/components/About/index.jsx
@@ -3,21 +3,18 @@ const About = () => {
     <section className="about">
       <h2>Sobre</h2>
       <p>
-        Sou um estudante na área de Desenvolvimento Full-stack, me formei
-        recentemente na área de Front-End adquirindo bom conhecimento nas
-        linguagens HTML5, CSS, JavaScript, React.JS e TypeScript. Estou
-        procurando minha primeira oportunidade no mercado de trabalho de
-        desenvolvimento não só por gostar da área, mas também pelo crescimento
-        no mercado. Sou um profissional dedicado, responsável e apaixonado por
-        tecnologia.
+      Atualmente cursando Análise e Desenvolvimento de Sistemas, 
+      e formado na área de Desenvolvimento Full-stack pela Kenzie academy Brasil, 
+      adquiri bom conhecimento nas linguagens HTML5, CSS, JavaScript, React.JS, TypeScript, 
+      Nodejs, Python e Django. Estou procurando minha primeira oportunidade no mercado de trabalho 
+      de desenvolvimento não só por gostar da área, mas também pelo crescimento no mercado. 
+      Sou um profissional dedicado, responsável e apaixonado por tecnologia.
       </p>
       <p>
-        Principais Competências: HTML5, CSS3, Web Semântica, JavaScript, lógica
-        de programação, controle de versão com git, Programação Orientada a
-        Objetos, Programação Funcional, Metodologias Ágeis, Scrum, Github,
-        React, Hooks, Componentização, uso de bibliotecas de componentes,
-        consumo de APIs, NodeJs, Express, TypeScript, Banco de dados SQL,
-        PostgreSQL.
+      Principais Competências: HTML5, CSS3, Web Semântica, JavaScript, lógica de programação, 
+      controle de versão com git, Programação Orientada a Objetos, Programação Funcional, Metodologias Ágeis, 
+      Scrum, Github, React, Hooks, Componentização, uso de bibliotecas de componentes, consumo de APIs, NodeJs, 
+      Express, TypeScript, Banco de dados SQL, Python, Django, PostgreSQL.
       </p>
     </section>
   );
